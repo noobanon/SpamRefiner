e65b38c8b9a59e02ccec45443a4c69233195f92a
•/ ＭＲ ∆ＮOＮ <51384033+noobanon@users.noreply.github.com>
2021-05-29 16:59:50 +0530

    Update __main__.py

--- SpamRefiner/__main__.py
+++ SpamRefiner/__main__.py
@@ -1,1 +1,16 @@
-txt
+from sys import argv, exit
+from SpamRefiner import spam
+from SpamRefiner import TOKEN
+
+import SpamRefiner.events
+
+try:
+    spam.start(bot_token=TOKEN)
+except Exception:
+    print("Your Bot Token Isn't Invalid")
+    exit(1)
+
+if len(argv) not in (1, 3, 4):
+    spam.disconnect()
+else:
+    spam.run_until_disconnected()
