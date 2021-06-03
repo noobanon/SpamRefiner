327594e6caffc2411a52a7fdfaf8276b6244d39c
ÁÑÑÍHÌLÅTØR SPÄRK <tanmoyomg7@gmail.com>
2021-06-03 12:59:58 +0530

    Update _menu.py

--- SpamRefiner/nospam/_menu.py
+++ SpamRefiner/nospam/_menu.py
@@ -1,32 +1,32 @@
 from SpamRefiner import spam
 
 PM_START_TEXT = """Hi I'm a SpamRefiner Bot Built in Python Using Telethon For More Use /help"""
-@register(pattern="^/start$")
-async def start(event):
-    if not event.is_group:
-        await spam.send_message(
-            event.chat_id,
-            PM_START_TEXT,
-            buttons=[
-                [
-                    Button.url(
-                        "Add To Group  👥", "t.me/@SpamRefineRobot?startgroup=true"
-                    ),
-                    
-                ],
-                                                                                    
-            ],
-        )
-    else:
-        await event.reply("Hey I'm Alive")
 
-HELP_TEXT = """ Hi """
-@register(pattern="^/help$")
+@spam.on(events.NewMessage(pattern="^[!/]start$")
+async def start(event):
+  if not event.is_group:
+    await spam.send_message(
+      event.chat_id,
+      PM_START_TEXT,
+      buttons=[
+        [
+          Button.url(
+            "Add To Group  👥", "t.me/SpamRefineRobot?startgroup=true"
+            ),
+          ],
+        ],
+      )
+  else:
+    await event.reply("Hey I'm Alive")
+
+HELP_TEXT = """Hi"""
+
+@spam.on(events.NewMessage(pattern="^[!/]help$")
 async def help(event):
-    if not event.is_group:        
-        await event.reply(HELP_TEXT)
-    else:
-        await event.reply(
-            "Contact me in PM to get the help menu",
-            buttons=[[Button.url("Help ", "t.me/SpamKiller?start=help")]],
-        )
+  if not event.is_group:    
+    await event.reply(HELP_TEXT)
+  else:
+    await event.reply(
+      "Contact me in PM to get the help menu",
+      buttons=[[Button.url("Help ", "t.me/SpamKiller?start=help")]],
+      )
