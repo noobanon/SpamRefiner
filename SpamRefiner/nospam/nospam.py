5a1574214927db3058443a018af496cd6704f727
•/ ＭＲ ∆ＮOＮ <51384033+noobanon@users.noreply.github.com>
2021-06-04 11:52:32 +0530

    Update nospam.py

--- SpamRefiner/nospam/nospam.py
+++ SpamRefiner/nospam/nospam.py
@@ -228,9 +228,9 @@ async def del_sell(event):
             hh = sender.id
             final = f"[{st}](tg://user?id={hh}) **{msg}** is detected as a slang word and your message has been deleted"
           else:
-            final = f'@{let} **{msg}** is detected as a slang word and your message has been deleted'
+            final = f'@{let} **{msg}** is Detected as a selling word and your message has been deleted'
             dev = await event.respond(final)
-            await asyncio.sleep(5)
+            await asyncio.sleep(3)
             await dev.delete()
 
 
